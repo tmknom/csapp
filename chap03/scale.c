6d6187ac6028a95ba68b12b615f19069ad371cb0
tmknom <tmk.nom@gmail.com>
2021-02-23 07:41:32 +0900

    scale.cの実装

---
+++ chap03/scale.c
@@ -0,0 +1,9 @@
+long scale(long x, long y, long z) {
+    long t = x + 4 * y + 12 * z;
+    return t;
+}
+
+long scale2(long x, long y, long z) {
+    long t = 5 * x + 2 * y + 8 * z;
+    return t;
+}
